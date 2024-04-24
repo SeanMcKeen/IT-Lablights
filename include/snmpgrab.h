9800fb629650c5f42ec0baa39bf4ca9f5a4a6276
SeanMcKeen <41005485+SeanMcKeen@users.noreply.github.com>
2024-04-24 08:04:38 -0400

    Delete include/snmpgrab.h

--- include/snmpgrab.h
+++
@@ -1,7 +0,0 @@
-extern int arrTotals[4][2];
-
-void SNMPsetup(int Array[], int sizeOfArray);
-void snmpLoop(int Array[], int arrayCount, int arrayIndex);
-void printVariableHeader();
-void printVariableFooter();
-void callLoop();
